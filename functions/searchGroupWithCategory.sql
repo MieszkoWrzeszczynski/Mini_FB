03054a0c7c07836fdafaefc79f1cadfe92d9011b
Mieszko Wrzeszczyński <mieszkobor@op.pl>
2017-01-23 11:38:52 +0100

    add searchGroup function

---
+++ functions/searchGroupWithCategory.sql
@@ -0,0 +1,23 @@
+IF object_id(N'searchGroupWithCategory', N'TF') IS NOT NULL
+    DROP FUNCTION searchGroupWithCategory
+GO
+
+create function searchGroupWithCategory
+        (@category varchar(MAX))
+        returns @Groups table
+                (id int ,title nvarchar(255) , content NTEXT)
+as
+begin
+        declare @catID int
+		set @catID = (select id from tblCategories WHERE title = @category)
+
+		insert into @Groups
+        select id, title, content from tblGroups where catID in (@catID)
+return
+end
+
+go
+
+-- wywolanie:
+select * from searchGroupWithCategory('sprzątanie')
+select * from searchGroupWithCategory('gotowanie')
