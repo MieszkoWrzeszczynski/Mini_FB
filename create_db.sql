9acdaef1ccb4542e3148a43af4f1254319a75b51
adadom@st.amu.edu.pl <adam.domagalski1994@gmail.com>
2017-01-24 20:59:00 +0100

    A Few inserts more...

--- create_db.sql
+++ create_db.sql
@@ -172,28 +172,48 @@ CREATE TABLE tblFriendships (
 ----------------------- INSERTS
 
 -- LOCATION
-INSERT INTO tblLocations VALUES('63-300','Poznań','Wielkopolskie','Polska')
-INSERT INTO tblLocations VALUES('64-300','Warszawa','Mazowieckie','Polska'),('66-400','Gorzów Wlkp.','Lubuskie','Polska')
+INSERT INTO tblLocations VALUES('63-300','Poznań','Wielkopolskie','Polska'),('10-005','New York','New York','USA')
+INSERT INTO tblLocations VALUES('64-300','Warszawa','Mazowieckie','Polska'),('66-400','Gorzów Wielkopolski','Lubuskie','Polska')
+INSERT INTO tblLocations VALUES('12-432','Frankfurt','Oder','Niemcy'),('10-125','Last Vegas','LA 4 Life','USA')
+INSERT INTO tblLocations VALUES('02-231','Bangladesz','Alma','Syria'),('98-404','Szczecin','Zachodnio Pomorskie','Polska')
 
 -- USERS
-INSERT INTO tblUsers VALUES('Mieszko','Wrzeszczyński','mieszkobor@op.pl','1231231asadsad','123123123',1,DEFAULT,'63-300')
-INSERT INTO tblUsers VALUES('Adam','Domagalski','adam@op.pl','1231231','1232',1,DEFAULT,'66-400')
-INSERT INTO tblUsers VALUES('Filip','Hajzer','synzygmunta@wp.pl','hasuo','693258741',1,DEFAULT,'64-300'),('Krystyna','Czubówna','lubiegadac@gmail.com','zwierzaczkipolaczki','987546321',0,DEFAULT,'64-300')
+INSERT INTO tblUsers VALUES('Mieszko','Wrzeszczyński','mieszkobor@op.pl','1231231ad','123123123',1,DEFAULT,'63-300')
+INSERT INTO tblUsers VALUES('Adam','Domagalski','adam@op.pl','1231231','1232',1,DEFAULT,'66-400'),('Casey','Neistat','ny@beme.com','1231231','1232',1,DEFAULT,'10-005')
+INSERT INTO tblUsers VALUES('Filip','Hajzer','synzygmunta@wp.pl','hasuo','693258741',1,DEFAULT,'64-300'),('Krystyna','Czubówna','lubiegadac@gmail.com','zwierzaczkipolaczki','9845636321',0,DEFAULT,'64-300')
+INSERT INTO tblUsers VALUES('Paweł','Rajtuz','rajtuz@siema.pl','psw123','456213654',1,DEFAULT,'64-300'),('Natalia','Airduster','powietrze@yahoo.com','sdklhvsaw4234','987546321',0,DEFAULT,'64-300')
+INSERT INTO tblUsers VALUES('Diana','Zacna','zacnemidowe@wp.pl','zaqwsx','694328741',0,DEFAULT,'64-300'),('Kasia','Namalowana','rysunek@spamffree.com','324234oij','96456321',0,DEFAULT,'64-300')
+INSERT INTO tblUsers VALUES('Maciej','Pokład','wsiadajnapoklad@wp.pl','asdqwe','693258741',1,DEFAULT,'64-300'),('Kazik','Gortat','goryl@grf.com','2341sdfsdf','987546321',1,DEFAULT,'64-300')
+INSERT INTO tblUsers VALUES('Łysy','Łysy','łysolyolo@wp.pl','gfh46dfg','876258741',1,DEFAULT,'64-300'),('Mirjan','Mufią','maff@gmail.com','314234afsdf','987546321',0,DEFAULT,'64-300')
+INSERT INTO tblUsers VALUES('Babcia','Jadzia','babcia@wp.pl','sdf897s','693678655',0,DEFAULT,'64-300'),('Pafeu','Piotrowski','piotripawel@llfzlthz.com','123123asf','912346321',1,DEFAULT,'64-300')
+INSERT INTO tblUsers VALUES('Filip','Klawiatura','kalish@wp.pl','wrwexc9872!','457829403',1,DEFAULT,'64-300'),('Marek','Czarek','pieczarek@pizza.com','213gd!@#','921546321',1,DEFAULT,'64-300')
 
 
 -- CATEGORIES
-INSERT INTO tblCategories VALUES('gotowanie')
-INSERT INTO tblCategories VALUES('sprzątanie')
+INSERT INTO tblCategories VALUES('kuchnia'),('motoryzacja'),('it'),('polityka'),('rozrywka'),('sprzątanie')
+INSERT INTO tblCategories VALUES('sport'),('turystyka'),('zakupy'),('praca'),('alkohole'),('społeczeństwo')
 
 -- TAGS
-INSERT INTO tblTags VALUES('schabowe',1)
-INSERT INTO tblTags VALUES('mop',2)
--- Relationships
-INSERT INTO tblRelationships VALUES('Przyjaciele'),('Wrogowie'),('Byli znajomi'),('Żona'),('Brat')
+INSERT INTO tblTags VALUES('zupa',1),('szybkiedanie',1),('azjatykca',1),('europejska',1),('desery',1),('dieta',1)
+INSERT INTO tblTags VALUES('motor',2),('auto',2),('BMW',2),('AUDI',2),('opony',2),('prędkość',2),('adrenalina',2)
+INSERT INTO tblTags VALUES('python',3),('C++',3),('T-SQL',3),('webdev',3),('bash',3),('linux',3),('windows',3)
+INSERT INTO tblTags VALUES('lewactwo',4),('PO',4),('PiS',4),('Trump',4),('500+',4),('cotusiedzieje',4),('masakra',4)
+INSERT INTO tblTags VALUES('muzyka',5),('film',5),('joemonser',5),('kabaret',5),('youtube',5),('gry',5),('MMA',5)
+INSERT INTO tblTags VALUES('ciff',6),('papierrecznikowy',6),('recznikpapierowy',6),('kret',6),('płynnaślimaki',6)
+INSERT INTO tblTags VALUES('bieganie',7),('siłownia',7),('rower',7),('pływanie',7),('nordickwalkingxD',7),('tenis',7)
+INSERT INTO tblTags VALUES('grecja',8),('turcja',8),('hiszpania',8),('tajlandia',8),('UK',8),('egipt',8)
+INSERT INTO tblTags VALUES('ubrania',9),('spożywka',9),('HiTech',9),('cebuladeals',9),('aliexpress',9),('allegro',9)
+INSERT INTO tblTags VALUES('konsultant',10),('sprzedawczyk',10),('sprzątaczka',10),('kierowca',10),('leń',10)
+INSERT INTO tblTags VALUES('whisky',11),('vodka',11),('piwo',11),('wino',11),('drinki',11),('bimberniebiber',11)
+INSERT INTO tblTags VALUES('snapchat',12),('facebook',12),('instagram',12),('twitter',12),('linkedln',12),('reddit',12)
 
+-- Relationships
+INSERT INTO tblRelationships VALUES('Przyjaciele'),('Wrogowie'),('Byli znajomi'),('Żona'),('Brat'),('Ziomek z przedszkola')
+INSERT INTO tblRelationships VALUES('Konfident'),('Znajomy którego nie widziałem na oczy xD'),('Współlokator syfiarz')
 -- FREINDSHIPS
-INSERT INTO tblFriendships VALUES(1,2,0,1,1,DEFAULT)
-INSERT INTO tblFriendships VALUES(1,3,0,1,1,DEFAULT)
+INSERT INTO tblFriendships VALUES(1,2,0,1,1,DEFAULT),(1,3,0,1,1,DEFAULT),(1,4,0,1,1,DEFAULT),(1,5,0,1,1,DEFAULT),(1,6,0,1,1,DEFAULT),(1,7,0,1,1,DEFAULT),(1,8,0,1,1,DEFAULT),(1,9,0,1,1,DEFAULT),(1,10,0,1,1,DEFAULT),(1,11,0,1,1,DEFAULT)
+INSERT INTO tblFriendships VALUES(2,3,0,1,1,DEFAULT),(2,3,0,1,1,DEFAULT),(3,5,0,1,1,DEFAULT),(3,7,0,1,1,DEFAULT),(7,4,0,1,1,DEFAULT),(11,3,0,1,1,DEFAULT),(12,3,0,1,1,DEFAULT)
+
 
 --PRIVACY
  INSERT INTO tblPrivacy VALUES(1,'ALL'),(2,'FrOnly')
